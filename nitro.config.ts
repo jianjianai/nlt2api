46581905f388a774d09ca5ffe037940e5c60835a
jjaw <jjaw@jjaw.cn>
2026-08-15 23:42:03 +0800

    chore: initialize Nitro project

---
+++ nitro.config.ts
@@ -0,0 +1,8 @@
+import { defineNitroConfig } from "nitropack/config"
+
+// https://nitro.build/config
+export default defineNitroConfig({
+  compatibilityDate: "2026-08-15",
+  srcDir: "server",
+  imports: false
+});
